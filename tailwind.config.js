4d66b06dd3b7da2cb87954933c16c28cac68316a
jdnichollsc <jdnichollsc@hotmail.com>
2024-02-09 03:51:36 -0500

    fix tailwind base config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
-const { join } = require('path');
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
     ...createGlobPatternsForDependencies(__dirname),
   ],
   darkMode: 'class', // Use css classes for dark mode
