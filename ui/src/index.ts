212ffba1aeee38c6749659bb6315fd6cb4cb1dff
jdnichollsc <jdnichollsc@hotmail.com>
2024-02-09 03:41:01 -0500

    fix tailwindcss for storybook

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -1,4 +1,3 @@
-export * from './lib/ui/ui.component';
 export * from './lib/theme-button/theme-button.component';
 export * from './lib/header/header.component';
 export * from './lib/footer/footer.component';
--- ui/src/lib/header/header.component.stories.ts
+++ ui/src/lib/header/header.component.stories.ts
@@ -1,15 +1,15 @@
 import type { Meta, StoryObj } from '@storybook/angular';
-import { UiComponent } from './ui.component';
+import { HeaderComponent } from './header.component';
 
 import { within } from '@storybook/testing-library';
 import { expect } from '@storybook/jest';
 
-const meta: Meta<UiComponent> = {
-  component: UiComponent,
-  title: 'UiComponent',
+const meta: Meta<HeaderComponent> = {
+  component: HeaderComponent,
+  title: 'HeaderComponent',
 };
 export default meta;
-type Story = StoryObj<UiComponent>;
+type Story = StoryObj<HeaderComponent>;
 
 export const Primary: Story = {
   args: {},
@@ -19,6 +19,6 @@ export const Heading: Story = {
   args: {},
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
-    expect(canvas.getByText(/ui works!/gi)).toBeTruthy();
+    expect(canvas.getByTestId(/header/gi)).toBeTruthy();
   },
 };
